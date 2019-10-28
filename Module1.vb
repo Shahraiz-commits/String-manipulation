b486128ae1fb0990c01ff34fe28710e5cf8c604f
Shahraiz-commits <55921113+Shahraiz-commits@users.noreply.github.com>
2019-10-28 18:51:40 +0500

    Monthly test 2 AS-C

--- Module1.vb
+++ Module1.vb
@@ -1,32 +1,50 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim FinalSum As Integer
+        Dim Count2 As Integer
+        Dim NextChar2 As Char
+        Dim ValidBinaryString As Boolean
+        Dim NextChar As Char
         Dim Str1 As String
-        Dim Char1 As Char
-        Dim Digits, Letters, Count As Integer
-
-        Count = 0
-        Digits = 0
-        Letters = 0
-        Char1 = ""
+        Dim Count As Integer
+        Dim X As Integer
+        X = 0
         Str1 = ""
-
+        NextChar = ""
+        Count = 0
+        ValidBinaryString = True
+        FinalSum = 0
+        Count2 = 0
+        NextChar2 = ""
         Console.Write("Enter string: ")
         Str1 = Console.ReadLine
+        If Len(Str1) <> 8 Then
+            ValidBinaryString = False
+        Else : For Count = 1 To Len(Str1)
+                NextChar = Mid(Str1, Count, 1)
+                If NextChar <> "0" And NextChar <> "1" Then ValidBinaryString = False
+            Next
+        End If
+        If ValidBinaryString = False Then
+            Console.Write("Not a valid binary string")
+            Console.ReadKey()
+        Else : For Count2 = 1 To Len(Str1)
+                NextChar2 = Mid(Str1, Count2, 1)
+                If Count2 = 1 Then X = Val(NextChar2) * 128
+                If Count2 = 2 Then X = Val(NextChar2) * 64
+                If Count2 = 3 Then X = Val(NextChar2) * 32
+                If Count2 = 4 Then X = Val(NextChar2) * 16
+                If Count2 = 5 Then X = Val(NextChar2) * 8
+                If Count2 = 6 Then X = Val(NextChar2) * 4
+                If Count2 = 7 Then X = Val(NextChar2) * 2
+                If Count2 = 8 Then X = Val(NextChar2) * 1
+                FinalSum = FinalSum + X
+            Next
+            Console.Write("The denary equivalent is" & " " & FinalSum)
+            Console.ReadKey()
+        End If
 
-        For Count = 1 To Len(Str1)
-            Char1 = Mid(Str1, Count, 1)
-
-            If Asc(Char1) >= 65 Then
-                Letters = Letters + 1
-            Else : Digits = Digits + 1
-
-
-            End If
-
-        Next
-        Console.Write("Letters are " & Letters & " and digits are " & Digits)
-        Console.ReadKey()
 
     End Sub
 
